fa097f7c3c852caeea29774c891a79f7c7b8630b
Memo Chou <memochou1993@gmail.com>
2021-06-05 15:19:07 +0800

    Change icons

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   pwa: {
     name: 'Time Clock',
+    themeColor: '#2196F3',
+    msTileColor: '#2196F3',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
   },
   transpileDependencies: [
     'vuetify',
